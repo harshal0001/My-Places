a1e43003fc9887ee2a109e192653303a0a352a90
Harshal <86665758+harshal0001@users.noreply.github.com>
2023-08-20 16:08:05 +0530

    project initialized

---
+++ lib/screens/places.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:my_places_app/screens/add_place.dart';
+import 'package:my_places_app/widgets/places_list.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Places'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => AddPlaceScreen(),
+                ),
+              );
+            },
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: PlacesList(
+        places: [],
+      ),
+    );
+  }
+}
