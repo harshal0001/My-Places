7c4da5033cd35cfec5e9051d9ad24937ae191971
Harshal <86665758+harshal0001@users.noreply.github.com>
2023-08-20 16:26:34 +0530

    Riverpod State Management Added

---
+++ lib/providers/user_places.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_places_app/models/places.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+
+  void addPlace(String title) {
+    final newPlace = Place(title: title);
+    state = [newPlace, ...state];
+  }
+}
+
+final userPlacesProivder =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+  (ref) => UserPlacesNotifier(),
+);
--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_places_app/providers/user_places.dart';
 
-class AddPlaceScreen extends StatefulWidget {
+class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
 
   @override
-  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+  ConsumerState<AddPlaceScreen> createState() => _AddPlaceScreenState();
 }
 
-class _AddPlaceScreenState extends State<AddPlaceScreen> {
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+
+  void _savePlace() {
+    final enteredPlace = _titleController.text;
+    if (enteredPlace.isEmpty) {
+      return;
+    }
+    ref.read(userPlacesProivder.notifier).addPlace(enteredPlace);
+    Navigator.of(context).pop();
+  }
 
   @override
   void dispose() {
@@ -36,7 +47,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
             ),
             const SizedBox(height: 16),
             ElevatedButton.icon(
-              onPressed: () {},
+              onPressed: _savePlace,
               icon: Icon(Icons.add),
               label: Text('Add Place'),
             ),
